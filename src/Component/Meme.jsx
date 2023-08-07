5c9b2a695ddbe7e645bd98e56e5d8421151d61c0
yash prajapati <prajapatiyash8817@gmail.com>
2023-08-07 23:56:43 +0530

    new changes to the project

--- src/Component/Meme.jsx
+++ src/Component/Meme.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import memeData from "../memeData";
 
 function Meme()
 {
+
+    let url
+    function getMemeImage()
+    {
+        const memesArray =  memeData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+
+        url = memesArray[randomNumber].url
+
+    }
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input 
                 type='text' 
                 className="form--input" 
@@ -15,8 +27,8 @@ function Meme()
                 className="form--input"
                 placeholder="Bottom text"
                 />
-                <button className="form--button">Get a new meme image </button>
-            </form>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image </button>
+            </div>
         </main>
     )
 }
